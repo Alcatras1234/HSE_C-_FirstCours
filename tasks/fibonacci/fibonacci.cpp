2611f892993f9776a10655d0b2be69230275cfbd
Vasya Pupkin <vasya@pupkin.ru>
2023-04-13 15:42:33 +0300

    ver3

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -3,7 +3,7 @@
 int64_t CalculateNthFibonacci(int64_t n) {
     int64_t prev = 0;
     int64_t curr = 1;
-    for (int64_t i = 0; i < n; ++i){
+    for (int64_t i = 0; i < n; ++i) {
         int64_t temp = prev + curr;
         prev = curr;
         curr = temp;
