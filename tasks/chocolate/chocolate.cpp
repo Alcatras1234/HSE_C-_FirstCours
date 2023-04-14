8b7e3bccca06a7b14b2b7214355929b0cfc35ff0
Vasya Pupkin <vasya@pupkin.ru>
2023-04-14 21:09:46 +0300

    ver1

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,5 +1,10 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    return true;
-};
+    if (slices_amount % height == 0 || slices_amount % width == 0) {
+        if (slices_amount < height * width) {
+            return true;
+        }
+    }
+    return false;
+}
