c6a2fb23889b8402a2fc072a0129244bd7dd3bb3
Vasya Pupkin <vasya@pupkin.ru>
2023-04-14 21:01:52 +0300

    ver1

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,5 +1,18 @@
 #include "min_divisor.h"
 
+#include "math.h"
+
 int64_t MinDivisor(int64_t number) {
-    return 2;
+    int64_t a = 1;
+    int64_t sqrta = static_cast<int64_t>(round(sqrt(number)));
+    for (int64_t i = 2; i <= sqrta; ++i) {
+        if (number % i == 0) {
+            a = i;
+            break;
+        }
+    }
+    if (a == 1) {
+        a = number;
+    }
+    return a;
 }
