c19d579ef29dec75d6a54e5cbf7b8437583488c1
Vasya Pupkin <vasya@pupkin.ru>
2023-04-14 21:18:50 +0300

    ver1

--- tasks/sum_digits/sum_digits.cpp
+++ tasks/sum_digits/sum_digits.cpp
@@ -2,5 +2,10 @@
 
 
 int64_t SumOfDigits(int64_t number) {
-    return 0;
+    int64_t sum = 0;
+    while (number > 0) {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
 }
