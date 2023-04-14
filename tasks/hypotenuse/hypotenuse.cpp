4aecc83e3f80a52f60fb70d32a0c9292f7f60754
Vasya Pupkin <vasya@pupkin.ru>
2023-04-14 08:46:54 +0300

    ver7

--- tasks/hypotenuse/hypotenuse.cpp
+++ tasks/hypotenuse/hypotenuse.cpp
@@ -1,6 +1,6 @@
 #include "hypotenuse.h"
 
 double CalculateHypotenuse(int64_t a, int64_t b) {
-    double c = static_cast<double_t>(sqrt(static_cast<double_t>(pow(a, 2)) + static_cast<double_t>(pow(b, 2))));
+    double c = static_cast<double_t>(sqrt((pow(a, 2)) + (pow(b, 2))));
     return c;
 };
