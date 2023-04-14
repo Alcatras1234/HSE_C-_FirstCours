21bccd32f050f538fa12a31fe917314b980c79af
Vasya Pupkin <vasya@pupkin.ru>
2023-04-14 08:32:17 +0300

    ver4

--- tasks/hypotenuse/hypotenuse.cpp
+++ tasks/hypotenuse/hypotenuse.cpp
@@ -1,6 +1,6 @@
 #include "hypotenuse.h"
 
 double CalculateHypotenuse(int64_t a, int64_t b) {
-    double c = sqrt(pow(a, 2) + pow(b, 2));
+    double c = sqrt(double_t(pow(a, 2)) + double_t(pow(b, 2)));
     return c;
 };
