0c7fff685e986aed5d65a8568163f977b3ca4896
Vasya Pupkin <vasya@pupkin.ru>
2023-04-15 14:02:43 +0300

    ver2

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -6,17 +6,13 @@ int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
     int64_t sum = 0;
     int64_t a1 = numerator;
     int64_t a2 = denominator;
-    if (numerator == 1 || denominator == 1) {
-        sum += numerator + denominator;
-    }
-    int64_t i = 2;
-    while (a1 != 1 || a2 != 1) {
+    int64_t i = a1;
+    while (true) {
         if (a1 % i == 0 && a2 % i == 0) {
-            a1 /= i;
-            a2 /= i;
+            break;
         }
-        i++;
+        i--;
     }
-    sum += a1 + a2;
+    sum += a1 / i + a2 / i;
     return sum;
 }
