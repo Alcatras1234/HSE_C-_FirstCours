cf1eb316c8f20036374dfdfb38ffcc9261b6c0dc
Vasya Pupkin <vasya@pupkin.ru>
2023-04-19 16:52:51 +0300

    comver2

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,12 +1,12 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
-    const int FOURHUNDRED = 400;
-    const int HUNDRED = 100;
-    if (year % 4 == 0 && year % HUNDRED != 0) {
+    const int fourhundred = 400;
+    const int hundred = 100;
+    if (year % 4 == 0 && year % hundred != 0) {
         return true;
     }
-    if (year % FOURHUNDRED == 0) {
+    if (year % fourhundred == 0) {
         return true;
     }
     return false;
