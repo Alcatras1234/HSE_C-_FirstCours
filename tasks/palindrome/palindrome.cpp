5c5e679cc1457ada6c8b16141abb5c13290eae00
Vasya Pupkin <vasya@pupkin.ru>
2023-04-20 14:54:02 +0300

    ver1

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,23 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    std::__cxx11::basic_string<char>::size_type left = 0;
+    std::__cxx11::basic_string<char>::size_type right = str.size() - 1;
+    if (str.empty()) {
+        return true;
+    }
+    while (left < right) {
+        if (str[left] == ' ') {
+            left++;
+        }
+        if (str[right] == ' ') {
+            right--;
+        } else if (str[left] == str[right]) {
+            left++;
+            right--;
+        } else {
+            return false;
+        }
+    }
+    return true;
 }
