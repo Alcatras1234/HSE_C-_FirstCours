ea0cb578991f2d4d25de0b6a5c1e6a31599966e6
Vasya Pupkin <vasya@pupkin.ru>
2023-04-27 20:31:27 +0300

    ver1

--- tasks/factorization/factorization.cpp
+++ tasks/factorization/factorization.cpp
@@ -1,5 +1,23 @@
 #include "factorization.h"
 
 std::vector<std::pair<int64_t, int>> Factorize(int64_t x) {
-    return {};
+    std::vector<std::pair<int64_t, int>> a = std::vector<std::pair<int64_t, int>>();
+    int64_t copy = x;
+    for (int64_t i = 2; i * i <= x; ++i) {
+        int count = 0;
+        if (copy % i == 0) {
+            while (copy % i == 0) {
+                ++count;
+                copy = copy / i;
+            }
+            a.emplace_back(i, count);
+        }
+    }
+    if (copy != 1) {
+        a.emplace_back(copy, 1);
+    }
+    if (a.empty()) {
+        a.emplace_back(x, 1);
+    }
+    return a;
 }
