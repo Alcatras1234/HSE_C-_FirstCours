70bc43057997eb08773399411bbb31d94089a710
Vasya Pupkin <vasya@pupkin.ru>
2023-04-28 14:52:55 +0300

    ver5

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -7,15 +7,15 @@ bool ValidatePassword(const std::string& password) {
     int lowerchar = 0;
     int number = 0;
     int other = 0;
-    const int THERTYTHREE = 33;
-    const int ONEHUNDREDTWENTYSIX = 126;
+    const int thertythree = 33;
+    const int onehundredtwentysix = 126;
     const std::__cxx11::basic_string<char>::size_type low_length = 8;
     const std::__cxx11::basic_string<char>::size_type max_length = 14;
     if (password.length() >= low_length && password.length() <= max_length) {
         for (std::__cxx11::basic_string<char>::size_type i = 0; i < password.length(); ++i) {
             int passwordchar = password[i];
-            if (passwordchar < THERTYTHREE) {
-                if (passwordchar > ONEHUNDREDTWENTYSIX) {
+            if (passwordchar < thertythree) {
+                if (passwordchar > onehundredtwentysix) {
                     return false;
                 }
             }
