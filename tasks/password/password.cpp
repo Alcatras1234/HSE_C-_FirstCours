9bff30be04f7a6bedd45fe3f0c9a655cfd1b5d2f
Vasya Pupkin <vasya@pupkin.ru>
2023-04-28 14:42:15 +0300

    ver2

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,5 +1,33 @@
 #include "password.h"
 
+#include <ctype.h>
+
 bool ValidatePassword(const std::string& password) {
+    int upchar = 0;
+    int lowerchar = 0;
+    int number = 0;
+    int other = 0;
+    if (password.length() >= 8 && password.length() <= 14) {
+        for (std::__cxx11::basic_string<char>::size_type i = 0; i < password.length(); ++i) {
+            int passwordchar = password[i];
+            if (passwordchar < 33) {
+                if (passwordchar > 126) {
+                    return false;
+                }
+            }
+            if (std::islower(passwordchar)) {
+                lowerchar = 1;
+            } else if (std::isupper(passwordchar)) {
+                upchar = 1;
+            } else if (std::isdigit(passwordchar)) {
+                number = 1;
+            } else {
+                other = 1;
+            }
+        }
+    }
+    if (number + other + upchar + lowerchar >= 3) {
+        return true;
+    }
     return false;
 }
