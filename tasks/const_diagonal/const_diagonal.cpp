e3c68ca4177581a5e733d0d8b1e677e934572807
Vasya Pupkin <vasya@pupkin.ru>
2023-04-29 21:09:28 +0300

    ver1

--- tasks/const_diagonal/const_diagonal.cpp
+++ tasks/const_diagonal/const_diagonal.cpp
@@ -1,4 +1,17 @@
 #include "const_diagonal.h"
 
 void BuildDiagonalArray(TableType table) {
+    int number = 0;
+    for (int i = 0; i < COLUMNS; ++i) {
+        for (int j = 0; j - i <= 0; ++j) {
+            table[j][i - j] = number;
+            ++number;
+        }
+    }
+    for (int i = 1; i < LINES; ++i) {
+        for (size_t j = i; j < COLUMNS; ++j) {
+            table[j][COLUMNS - j + i - 1] = number;
+            ++number;
+        }
+    }
 }
