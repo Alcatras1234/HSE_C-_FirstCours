1cbec1b6d8bedda6f9716bd664fbcdc13c3a6b62
Vasya Pupkin <vasya@pupkin.ru>
2023-04-29 22:16:29 +0300

    ver3

--- tasks/variable_diagonal/variable_diagonal.cpp
+++ tasks/variable_diagonal/variable_diagonal.cpp
@@ -7,13 +7,13 @@ int** BuildDiagonalArray(size_t lines, size_t columns) {
     }
     int number = 0;
     for (size_t i = 0; i < columns; ++i) {
-        for (size_t j = 0; j - i <= 0; ++j) {
+        for (size_t j = 0; j <= i; ++j) {
             matrix[j][i - j] = number;
             ++number;
         }
     }
     for (size_t i = 1; i < lines; ++i) {
-        for (size_t j = i; j < lines; ++j) {
+        for (size_t j = i; j < columns; ++j) {
             matrix[j][columns - j + i - 1] = number;
             ++number;
         }
