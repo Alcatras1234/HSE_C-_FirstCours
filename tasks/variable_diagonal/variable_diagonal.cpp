5d1bf7d1b081e9fbe5e5fda4ead02e48055720f4
Vasya Pupkin <vasya@pupkin.ru>
2023-04-29 21:59:56 +0300

    ver1

--- tasks/variable_diagonal/variable_diagonal.cpp
+++ tasks/variable_diagonal/variable_diagonal.cpp
@@ -1,5 +1,22 @@
 #include "variable_diagonal.h"
 
 int** BuildDiagonalArray(size_t lines, size_t columns) {
-    return nullptr;
+    int** matrix = new int*[lines];
+    for (size_t i = 0; i < lines; ++i) {
+        matrix[i] = new int[columns];
+    }
+    int number = 0;
+    for (size_t i = 0; i < columns; ++i) {
+        for (size_t j = 0; j - i <= 0; ++j) {
+            matrix[j][i - j] = number;
+            ++number;
+        }
+    }
+    for (size_t i = 1; i < lines; ++i) {
+        for (size_t j = i; j < columns; ++j) {
+            matrix[j][columns - j + i - 1] = number;
+            ++number;
+        }
+    }
+    return matrix;
 }
