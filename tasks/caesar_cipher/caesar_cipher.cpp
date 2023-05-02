1239c6d198d2e6f1e91ecb9f90e0047b5a993ab0
Vasya Pupkin <vasya@pupkin.ru>
2023-05-02 15:06:30 +0300

    ver3

--- tasks/caesar_cipher/caesar_cipher.cpp
+++ tasks/caesar_cipher/caesar_cipher.cpp
@@ -7,7 +7,7 @@ std::string Decipher(const std::string& cipher, size_t shift) {
     std::string copy = cipher;
     size_t size = cipher.size() - 1;
     for (int i = 0; i < size; ++i) {
-        decihper[i] = static_cast<char>(copy[i] + shift);
+        decihper += static_cast<char>(copy[i] + shift);
     }
     return decihper;
 }
