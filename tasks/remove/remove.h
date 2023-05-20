c9dcf1b65af2899b4af669e9b075708442a37726
Vasya Pupkin <vasya@pupkin.ru>
2023-05-20 23:27:07 +0300

    ver3

--- tasks/remove/remove.h
+++ tasks/remove/remove.h
@@ -1,7 +1,7 @@
 #pragma once
 
 template <typename T, typename U>
-T Find_first(T begin, T end, U value) {
+T FindFirst(T begin, T end, U value) {
     while (begin != end) {
         if (*begin == value) {
             break;
@@ -14,7 +14,7 @@ T Find_first(T begin, T end, U value) {
 
 template <typename T, typename U>
 T Remove(T begin, T end, U value) {
-    T first = Find_first(begin, end, value);
+    T first = FindFirst(begin, end, value);
     if (first != end) {
         for (T i = first + 1; i != end; ++i) {
             if (*i != value) {
