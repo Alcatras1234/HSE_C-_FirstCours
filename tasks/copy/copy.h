e1418c2e3b93cc7f72afd9c09bdfd1d747e7568b
Vasya Pupkin <vasya@pupkin.ru>
2023-06-09 01:40:01 +0300

    ver1

--- tasks/copy/copy.h
+++ tasks/copy/copy.h
@@ -1,7 +1,20 @@
 #pragma once
 
 template <typename T, typename U>
-U Copy(T src_begin, T src_end, U dest);
+U Copy(T src_begin, T src_end, U dest) {
+    for (T i = src_begin; i != src_end; ++i) {
+        *dest = *i;
+        ++dest;
+    }
+    return dest;
+}
 
 template <typename T, typename U>
-U CopyBackward(T src_begin, T src_end, U dest);
+U CopyBackward(T src_begin, T src_end, U dest) {
+    while (src_begin != src_end) {
+        --dest;
+        --src_end;
+        *dest = *src_end;
+    }
+    return dest;
+}
