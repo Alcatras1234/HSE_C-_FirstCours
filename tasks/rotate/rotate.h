92ff8b0e7de9b03e813c30aa2c141c38c2531c84
Vasya Pupkin <vasya@pupkin.ru>
2023-06-09 18:00:27 +0300

    ver1

--- tasks/rotate/rotate.h
+++ tasks/rotate/rotate.h
@@ -1,7 +1,18 @@
 #pragma once
 
 template <typename Iterator>
-void Reverse(Iterator begin, Iterator end);
+void Reverse(Iterator begin, Iterator end) {
+   while(begin != end && begin != --end) {
+       auto temp = *begin;
+       *begin = *end;
+       *end = temp;
+       ++begin;
+   }
+}
 
 template <typename Iterator>
-void Rotate(Iterator begin, Iterator mid, Iterator end);
+void Rotate(Iterator begin, Iterator mid, Iterator end) {
+    std::reverse(begin, mid);
+    std::reverse(mid, end);
+    std::reverse(begin, end);
+}
