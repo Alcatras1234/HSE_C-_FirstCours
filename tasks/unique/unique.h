5e5bbbbee8b39dfae033bd7c6fef3bafc3959eef
Vasya Pupkin <vasya@pupkin.ru>
2023-06-09 11:38:42 +0300

    ver1

--- tasks/unique/unique.h
+++ tasks/unique/unique.h
@@ -1,4 +1,16 @@
 #pragma once
 
 template <typename T>
-T Unique(T begin, T end);
+T Unique(T begin, T end) {
+    if (begin == end) {
+        return end;
+    }
+    T end_second = begin;
+    while(begin != end) {
+        ++begin;
+        if (*end_second != *begin) {
+            *(++end_second) = *begin;
+        }
+    }
+    return end_second;
+}
