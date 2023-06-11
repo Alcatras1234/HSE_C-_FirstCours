50589328a9d5ba9322e6d49cf0cfefe8c219ab47
Vasya Pupkin <vasya@pupkin.ru>
2023-06-11 17:33:51 +0300

    ver1

--- tasks/unique-ptr/unique_ptr.h
+++ tasks/unique-ptr/unique_ptr.h
@@ -7,6 +7,43 @@ public:
 
     // see http://en.cppreference.com/w/cpp/memory/unique_ptr
     // and test.cpp
+    explicit UniquePtr(T* ptr = nullptr) : ptr_(ptr) {
+    }
+    ~UniquePtr() {
+        delete ptr_;
+    }
+
+    UniquePtr(const UniquePtr&) = delete;
+    UniquePtr& operator=(const UniquePtr&) = delete;
+
+    UniquePtr(UniquePtr&& other) : ptr_(other.ptr_) {
+        other.ptr_ = nullptr;
+    }
+    UniquePtr& operator=(UniquePtr&& other) {
+        if (this != &other) {
+            delete ptr_;
+            ptr_ = other.ptr_;
+            other.ptr_ = nullptr;
+        }
+        return *this;
+    }
+
+    T* operator->() const {
+        return ptr_;
+    }
+    T& operator*() const {
+        return *ptr_;
+    }
+    operator bool() const {
+        return ptr_ != nullptr;
+    }
+
+    void Reset(T* ptr = nullptr) {
+        if (ptr_ != ptr) {
+            delete ptr_;
+            ptr_ = ptr;
+        }
+    }
 
 private:
     T* ptr_;
