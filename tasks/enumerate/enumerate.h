fc44650d09e0d5f1bcd87e97b4a5a582acbc0275
Vasya Pupkin <vasya@pupkin.ru>
2023-06-22 22:05:59 +0300

    ver1

--- tasks/enumerate/enumerate.h
+++ tasks/enumerate/enumerate.h
@@ -1,19 +1,97 @@
 #pragma once
 
-template <class Iterator>
-class IteratorRange {
+#include <iterator>
+#include <type_traits>
+
+template <typename Iter>
+class Range {
 public:
-    IteratorRange(Iterator begin, Iterator end) : begin_(begin), end_(end) {
+    Range(Iter first, Iter last) : begin_(first), end_(last) {
     }
 
-    Iterator begin() const {  // NOLINT
+    Iter begin() const {
         return begin_;
     }
 
-    Iterator end() const {  // NOLINT
+    Iter end() const {
         return end_;
     }
 
 private:
-    Iterator begin_, end_;
+    Iter begin_, end_;
 };
+
+template <typename Iter, typename Idx = typename std::iterator_traits<Iter>::difference_type>
+class Enum {
+public:
+    explicit Enum(Range<Iter> range, Idx start = Idx{}) : range_(range), start_(start) {
+    }
+
+    class iterator {
+    public:
+        using iterator_category = typename std::iterator_traits<Iter>::iterator_category;
+        using value_type = std::pair<Idx, typename std::iterator_traits<Iter>::value_type>;
+        using difference_type = typename std::iterator_traits<Iter>::difference_type;
+        using ptr = typename std::iterator_traits<Iter>::pointer;
+        using ref = std::pair<Idx, typename std::iterator_traits<Iter>::reference>;
+
+        iterator(Iter it, Idx idx) : it_(it), index_(idx) {
+        }
+
+        iterator& operator++() {
+            ++it_;
+            ++index_;
+            return *this;
+        }
+
+        iterator operator++(int) {
+            auto temp(*this);
+            ++(*this);
+            return temp;
+        }
+
+        bool operator==(const iterator& other) const {
+            return it_ == other.it_;
+        }
+
+        bool operator!=(const iterator& other) const {
+            return !(*this == other);
+        }
+
+        ref operator*() const {
+            return {index_, *it_};
+        }
+
+        ptr operator->() const {
+            return std::addressof(operator*());
+        }
+
+    private:
+        Iter it_;
+        Idx index_;
+    };
+
+    iterator begin() const {
+        return {range_.begin(), start_};
+    }
+
+    iterator end() const {
+        return {range_.end(), start_ + std::distance(range_.begin(), range_.end())};
+    }
+
+private:
+    Range<Iter> range_;
+    Idx start_;
+};
+
+template <typename Container>
+auto Enumerate(Container&& cont) {
+    using std::begin, std::end;
+    return Enum{Range{begin(cont), end(cont)}};
+}
+
+template <typename Container>
+[[maybe_unused]] auto Enumerate(const Container&& cont) {
+    using std::cbegin, std::cend;
+    return Enum{Range{cbegin(cont), cend(cont)}};
+}
